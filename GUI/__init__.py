9b120433bf8c41a2988943b0d0f3a30ce00b536f
JLToledano <jl.toledano.2018@alumnos.urjc.es>
2024-08-05 17:13:15 +0200

    Add structure to hiperparameters of models in flask app

---
+++ GUI/__init__.py
@@ -0,0 +1,17 @@
+"""Initial configuration of the main application module"""
+import os
+import yaml
+
+
+def load_config_sbopinionanalysis() -> dict:
+    """
+    Function that loads the general configuration of the application
+    :return: Global constants of the application
+    :rtype: dict[String:String]
+    """
+    
+    configuration_path = os.path.dirname(__file__)
+    with open(os.path.join(configuration_path, 'config.yaml'), 'r', encoding='utf-8') as file:
+        info_config = yaml.safe_load(file)
+
+    return info_config
