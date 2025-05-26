dfe380b53b810b714ee4a44670b08371d5c2c647
JLToledano <jl.toledano.2018@alumnos.urjc.es>
2025-05-26 23:52:45 +0200

    Add ngrok to run app in colab

--- GUI/app.py
+++ GUI/app.py
@@ -3,15 +3,26 @@ import __init__ as init
 from views.routes import main_blueprint
 from api.routes import api_blueprint
 from api.model import model_bp
-
+from pyngrok import ngrok
 import sys
 import os
 
 #Add the current directory to sys.path to ensure local modules can be imported
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+#Detect if application is running in Google Colab
+IN_COLAB = 'google.colab' in sys.modules
 
 #Create Flask application instance
 app = Flask(__name__)
+
+# Si estamos en Colab, importamos pyngrok
+if IN_COLAB:
+    from pyngrok import ngrok
+    ngrok.set_auth_token("TU_AUTHTOKEN_AQUI") 
+    #Create a tunnel to the Flask app running on port 5000
+    public_url = ngrok.connect(5000)
+    print("Tu aplicación está disponible en:", public_url)
+
 app.secret_key = 'secretkey'  #Secret key for session management
 
 #Registering blueprints for modular application structure
@@ -34,4 +45,4 @@ app.config['test_dataset'] = test_dataset
 
 #Run Flask application
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
